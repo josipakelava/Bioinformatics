f13153dfd778905aa14fb24cff1e9f45a44d12cf
Tea Pović <tea.povic@fer.hr>
2017-11-02 16:18:31 +0100

    added bloom filter

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "lib/libbf/bf/all.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    bf::basic_bloom_filter b(0.8, 100);
+    // Add two elements.
+    b.add("foo");
+    b.add(42);
+
+    // Test set membership
+    std::cout << b.lookup("foo") << std::endl;  // 1
+    std::cout << b.lookup("bar") << std::endl;  // 0
+    std::cout << b.lookup(42) << std::endl;     // 1
+
+    // Remove all elements.
+    b.clear();
+    std::cout << b.lookup("foo") << std::endl;  // 0
+    std::cout << b.lookup(42) << std::endl;     // 0
+
     return 0;
 }
