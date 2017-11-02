51edd766c909df40b749b422645ddf33c8879004
Ivan Krpelnik <ikrpelnik1@gmail.com>
2017-11-02 13:01:55 +0100

    Added fasta input

---
+++ fasta_io.h
@@ -0,0 +1,11 @@
+#ifndef BIOINFORMATIKA_PROJEKT_FASTA_INPUT_H
+#define BIOINFORMATIKA_PROJEKT_FASTA_INPUT_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void read_fasta(const char *filePath, vector<string>& reads);
+
+#endif //BIOINFORMATIKA_PROJEKT_FASTA_INPUT_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "fasta_io.h"
+
+using namespace std;
+
+void fasta_test(int argc, char **argv) {
+    if (argc < 2) return;
+
+    vector<string> reads;
+    read_fasta(argv[1], reads);
+    int i = 0;
+    for(const auto& s : reads) {
+        cout << "Read " << i++ << endl;
+        cout << s << endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    fasta_test(argc, argv);
     return 0;
 }
